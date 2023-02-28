4984a36e7752849be469eacbce4d2301ee1966df
sgi <ppppk3413@gmail.com>
2023-02-28 17:41:39 +0900

    DB接続を改善

--- main.py
+++ main.py
@@ -17,27 +17,40 @@ cursor.execute(
     """
     CREATE TABLE IF NOT EXISTS violas (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(255),
-    botanical_name VARCHAR(255),
-    common_name VARCHAR(255),
+    classification VARCHAR(255),
+    basic_specie VARCHAR(255),
+    mutation VARCHAR(255),
+    alias VARCHAR(255),
+    origin VARCHAR(255),
+    foreign_generic_name VARCHAR(255),
+    stem_morphology VARCHAR(255),
+    distribution_japan  VARCHAR(255),
+    distribution_foreign VARCHAR(255),
+    distribution_supplement VARCHAR(255),
+    flower_shape VARCHAR(255),
     flower_color VARCHAR(255),
-    blooming_period VARCHAR(255),
+    flower_distance VARCHAR(255),
+    flower_season VARCHAR(255),
+    flower_style VARCHAR(255),
+    flower_supplement VARCHAR(255),
     leaf_shape VARCHAR(255),
-    leaf_edge VARCHAR(255),
-    leaf_arrangement VARCHAR(255),
-    leaf_surface VARCHAR(255),
-    leaf_texture VARCHAR(255),
-    height VARCHAR(255),
-    habitat VARCHAR(255),
-    distribution VARCHAR(255),
+    leaf_color VARCHAR(255),
+    leaf_supplement VARCHAR(255),
+    seed_shape VARCHAR(255),
+    seed_color VARCHAR(255),
+    seed_supplement VARCHAR(255),
+    root_features VARCHAR(255),
+    endangered_species VARCHAR(255),
+    reference_sample VARCHAR(255),
     description TEXT)
     """, table)
 
-# test_scraping.scraping()
+test_scraping.scraping()
 
-for viola in viola_list:
-    cursor.execute("INSERT INTO plant (name,botanical_name,common_name,flower_color,blooming_period ,leaf_shape,leaf_edge,leaf_arrangement ,leaf_surface,leaf_texture height ,habitat ,distribution ) VALUES (%s, %s)", (viola['名前'], viola['説明']))
-conn.commit()
+for viola in violas:
+    cursor.execute("INSERT INTO plant (classification, basic_specie, mutation, alias, origin, foreign_generic_name, stem_morphology, distribution_japan, distribution_foreign, distribution_supplement, flower_shape, flower_color, flower_distance,  flower_season, flower_style, flower_supplement, leaf_shape, leaf_color, leaf_supplement, seed_shape, seed_color, seed_supplement, root_features, endangered_species, reference_sample, description) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s,)", 
+    (viola['分類'], viola['基本種'], viola['変種'],viola['品種'],viola['異名'],viola['由来'],viola['外語一般名'],viola['茎の形態'] , viola['国内'], viola['海外'], viola['補足'], viola['形状'], viola['色'], viola['距'], viola['花期'], viola['花柱'], viola['補足'], viola['形状'], viola['色'], viola['補足'], viola['形状'], viola['色'], viola['補足'], viola['根の特徴'], viola['絶滅危惧種'], viola['基準標本'], viola['その他']))
+cnx.commit()
 
 @app.route('/') # はじめの処理
 def index():
