17b6a15f43aea3454efdf1bdc0a4c67822f4dd38
RabbitProgram <rabbitprogram.2018@gmail.com>
2023-04-15 03:30:15 +0900

    feat: フロントエンドのチャット風UIを作成

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,24 +1,40 @@
-import React from "react";
-import logo from "./logo.svg";
-import "./App.css";
+import { useState } from "react";
+import { Box, Grid, Paper, TextField, Button, Typography } from "@mui/material";
+import { CustomPaper } from "./CustomPaper";
+import SendIcon from "@mui/icons-material/Send";
 
 export const App = () => {
+  const [messageList, setMessageList] = useState<string[]>([]);
+  const [newMessage, setNewMessage] = useState("");
+
+  const handleMessageSubmit = () => {
+    setMessageList((prev) => [...prev, newMessage]);
+    setNewMessage("");
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Box sx={{ display: "flex", flexDirection: "column", height: "100vh" }}>
+      <Grid item xs={12}>
+        <CustomPaper>
+          <Typography variant="h4">Plant Akinator</Typography>
+        </CustomPaper>
+      </Grid>
+      <Box sx={{ flex: 1, overflow: "auto" }}>
+        {messageList.map((message) => (
+          <Paper sx={{ padding: "16px", margin: "8px" }}>{message}</Paper>
+        ))}
+      </Box>
+      <Box sx={{ display: "flex", padding: "8px" }}>
+        <TextField
+          sx={{ flex: 1, marginRight: "8px" }}
+          label="メッセージを入力"
+          variant="outlined"
+          value={newMessage}
+          onChange={(e) => setNewMessage(e.target.value)}
+        />
+        <Button variant="contained" onClick={handleMessageSubmit}>
+          <SendIcon />
+        </Button>
+      </Box>
+    </Box>
   );
 };
