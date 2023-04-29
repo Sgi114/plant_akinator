e5a0087f60cf9490fd47a48ff1597d1b1a275a49
RabbitProgram <rabbitprogram.2018@gmail.com>
2023-04-29 19:04:18 +0900

    feat: SQL末尾のコンマを取り除いてエラーを修正

--- backend/json_to_sql.py
+++ backend/json_to_sql.py
@@ -27,33 +27,36 @@ for data in json_data:
             continue
         column_name_list.append(data_key)
 
+column_sql_list = []
 for column_name in column_name_list:
-    sql = ("`"+column_name+"` TEXT,").replace("\n", "")
-    result_current_sql_list.append(sql)
+    sql = ("`"+column_name+"` TEXT").replace("\n", "")
+    column_sql_list.append(sql)
 
+result_current_sql_list.append(", ".join(column_sql_list))
 result_current_sql_list.append(");")
-result_sql_list.append(" ".join(result_current_sql_list))
+result_sql_list.append("".join(result_current_sql_list))
 result_sql_list.append("\n\n")
 
 # INSERT文を作成
-result_current_sql_list = ["INSERT INTO `violas` ("]
 for data in json_data:
-    # result_current_sql_list.append("`, `".join(data.keys())+"`) VALUES (")
+    result_current_sql_list = ["INSERT INTO `violas` ("]
+    column_sql_list = []
     for data_key in data.keys():
-        result_current_sql_list.append("`"+data_key+"`, ")
+        column_sql_list.append("`"+data_key+"`")
+    result_current_sql_list.append(", ".join(column_sql_list))
     result_current_sql_list.append(") VALUES (")
+    column_sql_list = []
     for data_key in data.keys():
         if(data_key == "image_list"):
             # data[data_key]をjson文字列化
             json_str = json.dumps(data[data_key], ensure_ascii=False)
-            result_current_sql_list.append('`'+json_str+'`, ')
+            column_sql_list.append('`'+json_str+'`')
             continue
-        result_current_sql_list.append(
-            '`'+data[data_key].replace("\n", "\\n")+'`, ')
-
-result_current_sql_list.append(");")
-result_sql_list.append(" ".join(result_current_sql_list))
-result_sql_list.append("\n\n")
+        column_sql_list.append('`'+data[data_key].replace("\n", "\\n")+'`')
+    result_current_sql_list.append(", ".join(column_sql_list))
+    result_sql_list.append(" ".join(result_current_sql_list))
+    result_sql_list.append(");")
+    result_sql_list.append("\n\n")
 
 # 結果を保存
 result_sql = " ".join(result_sql_list)
