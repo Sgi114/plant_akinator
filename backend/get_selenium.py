37194f901803a499f29ba604c3c658592f1e7566
RabbitProgram <rabbitprogram.2018@gmail.com>
2023-04-29 18:16:03 +0900

    refactor: パスの表記を修正

--- backend/get_selenium.py
+++ backend/get_selenium.py
@@ -13,13 +13,16 @@ from bs4 import BeautifulSoup
 
 
 PYTHON_DIR_PATH = os.path.dirname(os.path.abspath(__file__))  # pyファイルが置かれているパス
+WEBDRIVER_DIR_PATH = os.path.join(PYTHON_DIR_PATH, "webdriver")
+WEBDRIVER_TEMP_DIR_PATH = os.path.join(WEBDRIVER_DIR_PATH, "tmp")
+CHROMEDRIVER_PATH = os.path.join(WEBDRIVER_DIR_PATH, "chromedriver.exe")
+CHROMEDRIVER_TEMP_PATH = os.path.join(
+    WEBDRIVER_TEMP_DIR_PATH, "chromedriver.exe")
 
 
 def setup():
     webdriver_url = "https://chromedriver.storage.googleapis.com/"  # ウェブドライバーページ
     file_name = "chromedriver_win32.zip"  # Windows用のファイル名
-
-    executable_path = PYTHON_DIR_PATH + "\\webdriver\\chromedriver.exe"
 
     # Sereniumの初期設定
     options = webdriver.ChromeOptions()
@@ -27,11 +30,11 @@ def setup():
 
     # ChromeWebdriverファイルのパス指定
     try:
-        if os.path.isfile(executable_path) == False:
+        if os.path.isfile(CHROMEDRIVER_PATH) == False:
             raise FileNotFoundError()
 
         driver = webdriver.Chrome(
-            chrome_options=options, executable_path=executable_path)
+            chrome_options=options, executable_path=CHROMEDRIVER_PATH)
         driver.close()
     except (FileNotFoundError, SessionNotCreatedException) as e:
         if type(e) == SessionNotCreatedException:
@@ -46,8 +49,8 @@ def setup():
         response = requests.get(webdriver_url)
         soup = BeautifulSoup(response.text, "lxml-xml")  # lxmlのインストールが必要
 
-        if not os.path.exists(PYTHON_DIR_PATH+ "\\webdriver\\tmp\\"):
-            os.makedirs(PYTHON_DIR_PATH + "\\webdriver\\tmp\\")
+        if not os.path.exists(WEBDRIVER_TEMP_DIR_PATH):
+            os.makedirs(WEBDRIVER_TEMP_DIR_PATH)
 
         success_flg = False
         version_arr = {}
@@ -66,25 +69,25 @@ def setup():
             print("起動テストを開始します\t"+webdriver_url+version[1])
 
             # ダウンロードしたZIPファイルの書き出し
-            with open(PYTHON_DIR_PATH+ "\\webdriver\\tmp\\" + file_name, "wb") as file:
+            temmp_file_path = os.path.join(WEBDRIVER_TEMP_DIR_PATH, file_name)
+            with open(temmp_file_path, "wb") as file:
                 for chunk in zip_source.iter_content():
                     file.write(chunk)
 
             # ZIPファイルの解凍
-            with zipfile.ZipFile(PYTHON_DIR_PATH + "\\webdriver\\tmp\\" + file_name) as file:
-                file.extractall(PYTHON_DIR_PATH + "\\webdriver\\tmp\\")
+            with zipfile.ZipFile(temmp_file_path) as file:
+                file.extractall(WEBDRIVER_TEMP_DIR_PATH)
 
             try:
-                driver = webdriver.Chrome(chrome_options=options,
-                                          executable_path=PYTHON_DIR_PATH + "\\webdriver\\tmp\\chromedriver.exe")
-                if(os.path.isfile(PYTHON_DIR_PATH + "\\webdriver\\chromedriver.exe")):
-                    os.remove(PYTHON_DIR_PATH + "\\webdriver\\chromedriver.exe")
-                shutil.move(PYTHON_DIR_PATH + "\\webdriver\\tmp\\chromedriver.exe",
-                            PYTHON_DIR_PATH + "\\webdriver\\chromedriver.exe")
+                driver = webdriver.Chrome(
+                    chrome_options=options, executable_path=CHROMEDRIVER_TEMP_PATH)
+                if(os.path.isfile(CHROMEDRIVER_PATH)):
+                    os.remove(CHROMEDRIVER_PATH)
+                shutil.move(CHROMEDRIVER_TEMP_PATH, CHROMEDRIVER_PATH)
 
                 print("正常に起動しました。WebDriverを上書きします。")
 
-                shutil.rmtree(PYTHON_DIR_PATH + "\\webdriver\\tmp\\")
+                shutil.rmtree(WEBDRIVER_TEMP_DIR_PATH)
                 success_flg = True
                 break
             except SessionNotCreatedException as e:
@@ -94,7 +97,7 @@ def setup():
             print("WebDriverファイルの上書き中に例外が発生しました。処理を中断します")
 
     # ▼以降にWebDriverの処理を記述。。。
-    print("Good Luck!!")
+    print("ChromeWebDriverの準備が完了しました")
 
 
 if __name__ == "__main__":
