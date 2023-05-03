5beef5689a5c958883b724a795c0883810e29a64
sgi <ppppk3413@gmail.com>
2023-05-03 16:35:47 +0900

    feat:json形式で受け渡しできるように修正

--- backend/akinator.py
+++ backend/akinator.py
@@ -1,38 +1,47 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, jsonify
+import random
 
 
 def akinator(db):
-    first_question = '茎の形態は無茎種である。'
-    answer = request.args.get("answer")
-    # MySQLのクエリを生成して絞り込み
+    data = request.get_json()
+    answer = data['answer']
+    # db =  ここでデータベースに接続する
+
+    if 'question' not in data:
+        first_question = '茎の形態は無茎種である。'
+        question = first_question
+    else:
+        question = data['question']
+
+
+    
     if answer == 'Yes':
-        # 無茎種として絞り込み
-        db.execute(
-            "create view akinator as SELECT * FROM flowers WHERE stem_type = '無茎種'")
+        db.execute("UPDATE flowers SET active = 1 WHERE question = %s", (question,))
+    
     elif answer == 'No':
-        # 有茎種として絞り込み
-        result = db.execute(
-            "create view akinator as SELECT * FROM flowers WHERE stem_type = '有茎種'")
+        db.execute("UPDATE flowers SET active = 0 WHERE question = %s", (question,))
 
-# FIXME: 使い方不明
-# def akinator_seach(db):
-#     # sqlから要素をランダムに1つ取得
-#     question=db.execute("SELECT * FROM flowers ORDER BY RAND() LIMIT 0 OFFSET FLoor(RAND() * 6)rows")[0]
-#     if answer == 'Yes':
-#         cursor.execute("create view Akinator as select * from flowers where like %s", ('%'+question+'%',))
-#     elif answer == 'No':
-#         cursor.execute("create view Akinator as select * from flowers where not like %s", ('%'+question+'%',))
-#     result = cursor.fetchall()
-#     return result
+    result = akinator_search(db,question)
+    return jsonify(result=result , question=question)
 
-#     if len(result) > 1:
-#         result = akinator_seach()
-#     # 結果が0件の場合は見つからなかった旨のメッセージを返す
-#     elif len(result) == 0:
-#         result =  '植物は見つかりませんでした。'
-#     # 結果が1件の場合は結果を表示するテンプレートを返す
-#     elif len(result) == 1:
-#         result = cursor.fetchone()
-#         return result
-#     return render_template('Akinator.html',first_question=first_question,question
-#                             =question,result=result)
+
+def akinator_seach(db,question):
+    question = db.execute("SELECT * FROM flowers ORDER BY RAND() LIMIT 0 OFFSET FLoor(RAND() * 6)rows")[0]
+    if answer == 'Yes':
+        cursor.execute("create view Akinator as select * from flowers where like %s", ('%'+question+'%',))
+    elif answer == 'No':
+         cursor.execute("create view Akinator as select * from flowers where not like %s", ('%'+question+'%',))
+    result = cursor.fetchall()
+  
+
+    if len(result) > 1:
+         result = akinator_seach(db,question)
+     # 結果が0件の場合は見つからなかった旨のメッセージを返す
+    elif len(result) == 0:
+         result =  '植物は見つかりませんでした。'
+     # 結果が1件の場合は結果を表示するテンプレートを返す
+    elif len(result) == 1:
+         return result
+         
+
+    return result
