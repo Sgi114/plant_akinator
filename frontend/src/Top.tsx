694d560234bf7448c320b28818ea00e96b7b8e40
RabbitProgram <rabbitprogram.2018@gmail.com>
2023-05-07 15:14:49 +0900

    feat: メッセージ追加・スタイル修正

--- frontend/src/Top.tsx
+++ frontend/src/Top.tsx
@@ -1,8 +1,19 @@
+import { Box, Button, Typography } from "@mui/material";
 import { MainCard } from "./MainCard";
 import "./Top.css";
 import lamp_majin from "./lamp_majin.png";
 
 export const Top = () => {
+  const characterMessageList = [
+    "やあ、私は植物アキネーター。\n私が分からないバイオなんて、アキネーター失格じゃない？？",
+    "こんにちは！\n植物を制す者は、バイオを制す。",
+  ];
+
+  // 指定範囲の整数をランダムに返す
+  const getRandomInt = (max: number) => {
+    return Math.floor(Math.random() * max);
+  };
+
   return (
     <div className="container">
       {/* <div className="top-panel"></div>
@@ -18,9 +29,57 @@ export const Top = () => {
       </div>
       <div className="bottom-panel"></div> */}
       <MainCard>
-        <h2 className="card-title">PlantAkinator</h2>
-        <img src={lamp_majin} alt="Card Image" className="card-image" />
-        <button className="card-button">Button</button>
+        {/* <h1 className="card-title">PlantAkinator</h1> */}
+        <Typography variant="h2" gutterBottom style={{ fontWeight: 600 }}>
+          PlantAkinator
+        </Typography>
+        <Box
+          sx={{
+            display: "flex",
+            flexDirection: "row",
+            alignItems: "start",
+            // justifyContent: "center",
+          }}
+        >
+          <img src={lamp_majin} alt="character" className="card-image" />
+          <Box
+            sx={{
+              position: "relative",
+              // bottom: "auto",
+              top: 80,
+              left: -30,
+              // height: 100,
+              // transform: "translateX(-50%)",
+              backgroundColor: "white",
+              border: "1px solid black",
+              borderRadius: 5,
+              padding: 1.5,
+            }}
+          >
+            {characterMessageList[getRandomInt(characterMessageList.length)]
+              .split("\n")
+              .map((text) => (
+                <>
+                  {text}
+                  <br />
+                </>
+              ))}
+          </Box>
+        </Box>
+        {/* <button className="card-button">Button</button> */}
+        <Button
+          variant="contained"
+          size="large"
+          sx={{
+            fontWeight: 600,
+            fontSize: 20,
+            borderRadius: 50,
+            px: 8,
+            py: 2,
+          }}
+        >
+          Play
+        </Button>
       </MainCard>
     </div>
   );
